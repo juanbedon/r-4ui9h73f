b823d9b000714a8fda2b71721e7cec28d1ef8080
Juan <juanbedon26@hotmail.com>
2018-06-01 12:28:16 -0500

    Solución Ejercicio

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.word = this.word.bind(this);
+		this.state = { word: "" }
+	}
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.word}/>
+        <p className="repeater">{this.state.word}</p>
       </div>
     );
+  }
+
+  word(e) {
+  	this.setState({
+  		word: e.target.value
+  	});
   }
 }
 
